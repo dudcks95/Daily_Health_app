90d0479f6800db0e73aab0425993041330cd21bf
YeongChan Cho <yc1700@naver.com>
2022-07-05 10:44:07 +0900

    bottomNavibar 틀 완료
    bottomNavibar 틀 완료

---
+++ DailyHealth/app/src/main/java/com/example/dailyhealth/ui/run/RunFragment.java
@@ -0,0 +1,23 @@
+package com.example.dailyhealth.ui.run;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dailyhealth.R;
+
+public class RunFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_run, container, false);
+    }
+}
---
+++ daily_health_server/src/main/java/com/example/daily_health_server/DailyHealthServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.daily_health_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyHealthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyHealthServerApplication.class, args);
+	}
+
+}
