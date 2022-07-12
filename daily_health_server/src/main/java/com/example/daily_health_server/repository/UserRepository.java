3ea918c41f8f98ed629460cde3fc118d7fd5259e
yejin807 <zinyizhen8@gmail.com>
2022-07-12 11:09:12 +0900

    dd
    dd

--- daily_health_server/src/main/java/com/example/daily_health_server/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.daily_health_server.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.daily_health_server.model.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
